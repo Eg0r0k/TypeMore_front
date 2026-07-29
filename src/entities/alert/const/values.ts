fdbc8de3a7977f14ba9823540992630f677642fe
Eg0r0k <egokakill@gmail.com>
2026-07-29 18:31:16 +0300

    update: delete usless alert ui

--- src/entities/alert/const/values.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_ALERT_DURATION = 2000
-export const DEFAULT_ALERT_CLOSABLE = true
--- src/entities/alert/index.ts
+++
@@ -1,4 +0,0 @@
-export { useAlertStore } from './model/store'
-// An enum, so a VALUE export: consumers pass AlertType.Success at runtime.
-export { AlertType } from './types/alertData'
-export type { AlertData } from './types/alertData'
